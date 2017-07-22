a8430f05f6f0c64479d0e81e0f7532eb55dc46d9
Unknown <bernat.coma.puig@gmail.com>
2017-07-22 17:05:10 +0200

    xgboost for the win!

--- script.py
+++ script.py
@@ -8,6 +8,8 @@ from collections import Counter
 from nltk.corpus import stopwords
 from nltk import word_tokenize
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn import preprocessing
+import xgboost as xgb
 
 import pandas as pd
 import numpy as np 
@@ -16,7 +18,7 @@ import string
 import socket
 import pickle
 import operator
-import platform
+#import sys
 
 # Set Path to dataset folder 
 path_to_data = "dataset/"
@@ -33,7 +35,6 @@ test_text = pd.read_csv(path_to_data + "test_text",sep = '\|\|', header = None, 
 test_variants = pd.read_csv(path_to_data+"test_variants")
 test_df = pd.merge(test_variants, test_text, on='ID', how='left')
 test_df = test_df.set_index(['ID'])
-test_df['Classified_Class'] = np.zeros((test_df.shape[0], 1)) #Added 'Classified_Class' as the column for result
 del test_text, test_variants
 
 ##Build Corpus
@@ -46,15 +47,17 @@ for index, row in training_df.iterrows():
     class_example = row['Class']
 
     ### Tokenize and remove stopwords 
-    if platform.system() == 'Windows':
-        text_example_tokenize = nltk.word_tokenize(text_example.decode('utf-8'))
-    else:
-        text_example_tokenize = nltk.word_tokenize(text_example)
+   
+    #if platform.system() == 'Windows':
+    text_example_tokenize = nltk.word_tokenize(text_example.decode('utf-8'))
+    #else:
+    #text_example_tokenize = nltk.word_tokenize(text_example)
+
     test = [i.lower() for i in text_example_tokenize if i.lower() not in stop and i not in punctuations and i.isdigit() is False]
     c = Counter(test)
     
     ### Add to dict    
-    for word_occ in c.most_common(100): 
+    for word_occ in c.most_common(15): 
         if word_occ[0] not in dict_words[class_example-1]:
             dict_words[class_example-1][word_occ[0]] = word_occ[1]
         else:
@@ -62,13 +65,12 @@ for index, row in training_df.iterrows():
     print ("Working wih observation: ", index)
 
 ## To persist the dict_words object
-pickle.dump(dict_words, open("dict_word.pickle", "wb"))
- 
+#pickle.dump(dict_words, open("dict_word.pickle", "wb"))
+#dict_words = pickle.load(open("dict_word.pickle", "rb"))
+
 corpus = set()
 for i in range(0,9):
-    #words = sorted(dict_words[i].items(), key=operator.itemgetter(1), reverse=True)
     for word in dict_words[i]:        
-        #print(word)
         corpus.add(word)
 
 #Build trainX
@@ -79,19 +81,19 @@ for index, row in training_df.iterrows():
     class_example = row['Class']
 
     ### Tokenize and remove stopwords 
+    #if platform.system() == 'Windows':
+    text_example_tokenize = nltk.word_tokenize(text_example.decode('utf-8'))
+    #else:
     #text_example_tokenize = nltk.word_tokenize(text_example)
-    if platform.system() == 'Windows':
-        text_example_tokenize = nltk.word_tokenize(text_example.decode('utf-8'))
-    else:
-        text_example_tokenize = nltk.word_tokenize(text_example)
     test = [i.lower() for i in text_example_tokenize if i.lower() not in stop and i not in punctuations and i.isdigit() is False]
     c = Counter(test)
     
     ### Add to dict    
-    for word in c.most_common(100):  
+    for word in c.most_common(15):  
         trainX.iloc[index][word[0]] = word[1]
-    
     print ("Working wih observation: ", index)
+
+#pickle.dump(trainX, open("trainX.pickle", "wb"))
 
 #Build testX
 testX = pd.DataFrame(np.zeros((test_df.shape[0], len(corpus))), columns = corpus)
@@ -101,32 +103,45 @@ for index, row in test_df.iterrows():
 
     ### Tokenize and remove stopwords 
     #text_example_tokenize = nltk.word_tokenize(text_example)
-    if platform.system() == 'Windows':
-        text_example_tokenize = nltk.word_tokenize(text_example.decode('utf-8'))
-    else:
-        text_example_tokenize = nltk.word_tokenize(text_example)
+    #if platform.system() == 'Windows':
+    text_example_tokenize = nltk.word_tokenize(text_example.decode('utf-8'))
+    #else:
+    #text_example_tokenize = nltk.word_tokenize(text_example)
     test = [i.lower() for i in text_example_tokenize if i.lower() not in stop and i not in punctuations and i.isdigit() is False]
     c = Counter(test)
     
     ### Add to dict    
-    for word in c.most_common(100):  
+    for word in c.most_common(15):  
         testX.iloc[index][word[0]] = word[1]
     print ("Working wih observation 2: ", index)
 
-pickle.dump(testX, open("testX.pickle", "wb"))
-
+#pickle.dump(testX, open("testX.pickle", "wb"))
+#testX = pickle.load(open("testX.pickle", "rb"))
  
 trainY = training_df['Class']
-model = GradientBoostingClassifier(n_estimators=1000, learning_rate=1.0, max_depth=3, random_state=0)
+
+## Encode Labels and Add to testX and trainY : Gene & Variation
+le = preprocessing.LabelEncoder()
+le.fit(training_df['Gene'])
+trainX['Gene'] = le.transfo2rm(training_df['Gene'])
+le.fit(training_df['Variation'])
+trainX['Variation'] =le.transform(training_df['Variation'])
+le.fit(test_df['Gene'])
+testX['Gene'] =  le.transform(test_df['Gene'])
+le.fit(test_df['Variation'])
+testX['Variation'] = le.transform(test_df['Variation'])     
+     
+#model = GradientBoostingClassifier(n_estimators=1000, learning_rate=1.0, max_depth=3)
+#print "xgboost!"
+model = xgb.XGBClassifier()
 model.fit(trainX,trainY)
 output = model.predict_proba(testX)
-pickle.dump(model, open("model.pickle", "wb"))
-
 
 #Generate submission file
 tags = ["class1","class2","class3","class4","class5","class6","class7","class8","class9"]
 submission_data = pd.DataFrame(output, columns = tags)
 submission_data.to_csv(path_to_output + "submissionFile_" + strftime("%Y-%m-%d_%H-%M-%S", gmtime()) + "_" + socket.gethostname(),index_label="ID")
+                       
                        
 
 
